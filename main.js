4100f4f337031e684008418818c5e31b617a8836
ipelaezitec <i.pelaez@itecriocuarto.org.ar>
2018-06-17 16:55:46 -0300

    segundo commit, avance

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// EJEMPLO  EVAL const modelsByBrand = eval(`models${brands[f]}`);
 // DUDA //** la lista de elementos tiene que ser solo de los vehiculos que estan en el momento?  eso implicaria quitar el vehiculo de la lista, el registro en cambio seria siempre igual*/
 // DUDA  buscar 'xjfw2' : puedo llamar una función sin recibir lo que retorna?
 
-// TODO : mascara de patentes;
-  
+// LOG: el required en las etiquetas radio no funcionó, solucionar la experiencia usuario
+// duda , variables const y let
+
 const dashboard = document.querySelector(".dashboard");
 const checkin = document.querySelector(".checkin");
 const checkout = document.querySelector(".checkout");
@@ -19,9 +19,8 @@ let camionetaLimit = 4;
 let alertLimit = 2;
 
 
-let vehiclesArray=localStorage.getItem('vehicles') ? JSON.parse(localStorage.getItem('vehicles')) :{"vehicles":[],"register-in":[],"register-out":[]};
+let vehiclesArray=localStorage.getItem('vehicles') ? JSON.parse(localStorage.getItem('vehicles')) :{"vehicles":[],"registerOut":[]};
 
-/** parametrizar */
 let openDashboard = (event) =>{
   event.preventDefault();
   
@@ -49,10 +48,7 @@ const saveJson = () => {
   localStorage.setItem('vehicles', JSON.stringify(vehiclesArray)); 
 }
 
-
-/** Incrementa el contador y decide si hay espacio para otro vehiculo más  */
 let renderCount = (elementNum) =>{
-  
   let number =  document.querySelector(`.num-${elementNum.type}`);
   let radio =  document.querySelector(`.radio-${elementNum.type}`);
   let numberParsed = parseInt(number.innerHTML)+1;
@@ -60,10 +56,42 @@ let renderCount = (elementNum) =>{
 
   let pushItemFlag = false; 
 
-  
   if (numberParsed <= limite ) {
     number.innerHTML = numberParsed;
     pushItemFlag = true;
+    
+    let remainingSpace = Math.abs(numberParsed - limite);
+    if ( remainingSpace <= alertLimit) {
+      number.classList.add("color-alert");
+    }else{
+      number.classList.remove("color-alert");
+    }
+  }
+
+  /** Cambia el color si está en el limite */
+  if (numberParsed +1 > limite) {
+    number.classList.add("color-limit");
+    radio.classList.add("hidden");
+  }else{
+    radio.classList.remove("hidden"); // esto o :  radio.disabled = false; deberia probar radio.checked = false (ni idea si funciona)
+  }
+  
+  return pushItemFlag;
+}
+
+let substractCount = (elementNum) =>{
+    
+  let number =  document.querySelector(`.num-${elementNum.type}`);
+  // let radio =  document.querySelector(`.radio-${elementNum.type}`);
+  let numberParsed = parseInt(number.innerHTML)-1; // to -1
+  let limite = eval(`${elementNum.type}Limit`);
+
+  // let pushItemFlag = false; 
+
+  
+  if (numberParsed <= limite ) {
+    number.innerHTML = numberParsed;
+    // pushItemFlag = true;
     
     let remainingSpace = Math.abs(numberParsed - limite);
     if ( remainingSpace <= alertLimit) {
@@ -79,42 +107,17 @@ let renderCount = (elementNum) =>{
     number.classList.add("color-limit");
     radio.classList.add("hidden");
   }else{
-    radio.classList.remove("hidden"); // esto o :  radio.disabled = false; deberia probar radio.checked = false (ni idea si funciona)
+    // radio.classList.remove("hidden"); // esto o :  radio.disabled = false; deberia probar radio.checked = false (ni idea si funciona)
+    number.classList.remove("color-limit");
   }
   
   
-  return pushItemFlag;
+  // return pushItemFlag;
+  return ;
+
 }
 
-
-//** input del Check-in */
-let saveVehicle = (event) =>{
-  event.preventDefault();
-  // Tuve que modularizar en dos , because preventDefault
-  
-
-  // Formato del {json} */
-  let plateInput = document.getElementById("patente");
-  // TODO : BAJA PRIORIDAD -> consultar que la patente no este en la base de datos
-  let radioInput = document.querySelector('input[name=tipo]:checked').value
-  let timeIn = moment().format("H:mm");
-  dataFormat= {"patente":plateInput.value ,"type":radioInput, "timeIn": timeIn};
-  
-  // modifica los contadores y decide si sumar,crear y agregar los datos. */
-  let pushItemflag = renderCount(dataFormat);
-  if (pushItemflag) {
-    makeItem(dataFormat);
-    vehiclesArray.vehicles.push(dataFormat);
-    saveJson();
-  }
-
-  plateInput.value="";
-  plateInput.focus();
-}
-
-//** crea el elento <li> que será dinamicamente agregado a <ul> */
 const makeItem = (element) =>{
-
   // Ordenados por jerarquia */
   const listUl   = document.getElementById(`ul-${element.type}`);
   const flex      = document.createElement('div');
@@ -123,7 +126,7 @@ const makeItem = (element) =>{
   const hourP     = document.createElement('p');
 
   vehicleLi.className = "li-reg";
-  flex.className = "flex-reg";
+  flex.className = `flex-reg ${element.patente}`; 
 
   plateP.appendChild(document.createTextNode(element.patente));
   hourP.appendChild(document.createTextNode(element.timeIn));  
@@ -135,27 +138,103 @@ const makeItem = (element) =>{
   return
 }
 
-// const registerVehicle = (event) =>{
-//   event.preventDefault();
+const makeItemReg = (element) =>{
 
-//   let plateInput = document.getElementById("patente-reg").value;
-//   vehiclesArray.vehicles.forEach(plateItem => {
-//     if (plateItem.patente == plateInput) {
-//       // can add let tipo= plateItem.type ;  // can add monto total
-//       let timeOut = moment().format("H:mm");
-//       dataFormat= {"patente":plateInput, "timeOut": timeOut};
-      
-//     }
+  // Ordenados por jerarquia */
+  const listUl   = document.getElementById('ul-registro'); // esto cambió
+  const flex      = document.createElement('div');
+  const vehicleLi= document.createElement('li');
+  const plateP  = document.createElement('p');
+  const hourP     = document.createElement('p');
 
-//     break;
-//   });
+  vehicleLi.className = "li-reg";
+  flex.className = "flex-reg";
 
-// }
+  plateP.appendChild(document.createTextNode(element.patente));
+  hourP.appendChild(document.createTextNode(element.timeOut)); // esto cambió  
+  flex.appendChild(plateP); 
+  flex.appendChild(hourP); 
+  vehicleLi.appendChild(flex); 
+  listUl.appendChild(vehicleLi); 
+  
+  return
+}
+
+const alertMsj = (patente) =>{
+  let msj = document.querySelector(".notification-registro");
+  msj.classList.remove("hidden");
+  msj.innerHTML=`${patente} no está registrado`;
+  setTimeout(function(){
+  msj.classList.add("hidden");
+  },
+  5000);
+}
+
+let saveVehicle = (event) =>{
+  event.preventDefault();
+
+  let plateInput = document.getElementById("patente"); // TODO : BAJA PRIORIDAD -> consultar que la patente no este en la base de datos
+  let radioInput = document.querySelector('input[name=tipo]:checked').value;
+  let timeIn     = moment().format("H:mm");
+
+  let dataFormat = {"patente":plateInput.value ,"type":radioInput, "timeIn": timeIn};
+  
+  let pushItemflag = renderCount(dataFormat);
+  if (pushItemflag) {
+    makeItem(dataFormat);
+    vehiclesArray.vehicles.push(dataFormat);
+    saveJson();
+  }
+
+  plateInput.value="";
+  plateInput.focus();
+}
+
+
+const registerVehicle = (event) => {
+  event.preventDefault();
+
+  let plateInput = document.getElementById("patente-reg").value;
+  let allVehicles = vehiclesArray.vehicles;
+  let flag = false;
+
+  if  (allVehicles.length == 0){
+    alertMsj(plateInput);
+  }else{ 
+    for (let index = 0; index < allVehicles.length; index++) {
+      const vehicle = allVehicles[index];
+      if (vehicle.patente == plateInput){ 
+        flag = true;
+        let tipo       = vehicle.type ;  // necesito el tipo para saber que tarifa cobrar
+        let timeOut    = moment().format("H:mm");
+        let dataFormat = {"patente" : plateInput,"type" : tipo, "timeOut" : timeOut};
+        substractCount(dataFormat);
+        makeItemReg(dataFormat); 
+        vehiclesArray.registerOut.push(dataFormat);
+        allVehicles.splice(index,1);
+    
+        let liToDelete = document.querySelector(`.${vehicle.patente}`);
+        liToDelete.parentElement.remove();
+        saveJson();
+        // plateInput="";
+        // console.log(plateInput);
+        // plateInput.focus();
+        break;
+      } 
+    }
+  }
+  
+  if (!flag){ alertMsj(plateInput); }
+
+}
 
 let initializeJsonData = () =>{
   vehiclesArray.vehicles.forEach(itemJson => {
     renderCount(itemJson); // xjfw2
     makeItem(itemJson);
+  });
+  vehiclesArray.registerOut.forEach(regJson =>{
+    makeItemReg(regJson);
   });
 }
 
