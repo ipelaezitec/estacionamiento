7f6ce2ee9a9ad1ed43240a26cb09c5a3466168db
ipelaezitec <i.pelaez@itecriocuarto.org.ar>
2018-06-20 04:23:13 -0300

    modal agregado

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 
 // DUDA  buscar 'xjfw2' : puedo llamar una función sin recibir lo que retorna? (EJEMPLO : renderCount)
 // DUDA variables const y let
+// DUDA la plata total no se guarda porque no lo puse, deberia?
+// DUDA tratar el asunto de los radios
+// posibles mejoras : mascara de caracteres
 
-// LOG: el required en las etiquetas radio no funcionó, solucionar la experiencia usuario
+
+// TODO : ESTABLECER PRECIOS;
+// no patentes repetidas + mensaje error
 
 const dashboard = document.querySelector(".dashboard");
 const checkin = document.querySelector(".checkin");
 const checkout = document.querySelector(".checkout");
+const modalDOM = document.querySelector('.modal');
 
 let motoPrice = 80;
 let autoPrice = 100;
 let camionetaPrice = 120;
+let TotalMoney = 0 ;
 
 let motoLimit = 6;
 let autoLimit = 10;
 let camionetaLimit = 4;
 
 let alertLimit = 2;
+
+/// WIP
+let totalMoney;
+let dataFormatOutJson;
+let auxIndex;
+let plateInputReg = document.getElementById("patente-reg");
+/// END WIP 
+
 
 let vehiclesArray=localStorage.getItem('vehicles') ? JSON.parse(localStorage.getItem('vehicles')) :{"vehicles":[],"registerOut":[]};
 
@@ -28,9 +43,11 @@ let openBoard = (event,boardItem) =>{
   eval(`${boardItem}`).classList.remove('hidden');
 }
 
-const saveJson = () => {
-  localStorage.setItem('vehicles', JSON.stringify(vehiclesArray)); 
-}
+const hideModal = () =>{ modalDOM.classList.remove("is-active");}
+
+const showModal = () =>{ modalDOM.classList.add("is-active");}
+
+const saveJson = () => { localStorage.setItem('vehicles', JSON.stringify(vehiclesArray));}
 
 //** Controla el número renderizado ( el contador y color ) */
 let renderCount = (elementNum,counter=1,) =>{
@@ -87,10 +104,13 @@ const makeItem = (element, register=false) =>{
   return
 }
 
-const alertMsj = (patente) =>{
+
+const alertMsj = (patente,text) =>{
+  console.log("check,borrame");
+
   let msj = document.querySelector(".notification-registro");
   msj.classList.remove("hidden");
-  msj.innerHTML=`${patente} no está registrado en el ingreso`;
+  msj.innerHTML=`${patente} ${text}`;
   setTimeout(function(){
   msj.classList.add("hidden");
   },
@@ -98,67 +118,139 @@ const alertMsj = (patente) =>{
 }
 
 
+let checkPlate = (plateInput) => {
+  let plateExistFlag = true;
+  for (let i = 0; i < vehiclesArray.vehicles.length; i++) {
+    
+    const plateDB = vehiclesArray.vehicles[i].patente;
+    if (plateInput == plateDB) {
+      plateExistFlag = false;
+      break;
+    }
+  }
+
+  console.log("plateflag : "+plateExistFlag);
+  return plateExistFlag;
+}
+
 let saveVehicle = (event) =>{
   event.preventDefault();
 
-  let plateInput = document.getElementById("patente"); // TODO : BAJA PRIORIDAD -> consultar que la patente no este en la base de datos
-  let radioInput = document.querySelector('input[name=tipo]:checked').value;
-  let timeIn     = moment().format("H:mm");
 
-  let dataFormat = {"patente":plateInput.value ,"type":radioInput, "timeIn": timeIn};
+  let plateInput = document.getElementById("patente"); // TODO : BAJA PRIORIDAD -> consultar que la patente no este en la base de datos
+
+
+  if (checkPlate(plateInput.value)){
+    let radioInput = document.querySelector('input[name=tipo]:checked').value;
+    let timeIn     = moment().format("H:mm");
   
-  let pushItemflag = renderCount(dataFormat);
-  if (pushItemflag) {
-    makeItem(dataFormat);
-    vehiclesArray.vehicles.push(dataFormat);
-    saveJson();
+    let dataFormatIn = {"patente":plateInput.value ,"type":radioInput, "timeIn": timeIn};
+    
+    let pushItemflag = renderCount(dataFormatIn);
+    if (pushItemflag) {
+      makeItem(dataFormatIn);
+      vehiclesArray.vehicles.push(dataFormatIn);
+      saveJson();
+    }
+  }else{
+    alertMsj(plateInput.value,"Ya está en la lista.");
   }
 
   plateInput.value="";
   plateInput.focus();
 }
 
+//////////////////// WIP 
+const getTotalMoney = (timeIn,timeOut,type) =>{
+  /* moment.js */
+  let start = moment.duration(timeIn, "HH:mm");
+  let end = moment.duration(timeOut, "HH:mm");
+  let diff = end.subtract(start);
+
+  let hours = diff.hours();
+  if (diff.minutes() < 31){
+    hours = hours+0.5;
+  }else{
+    hours = hours+1;
+  }
+  
+  totalMoney = hours * eval(`${type}Price`);
+  console.log("en testing  hours :"+hours);
+  console.log("en testing  totalMoney :"+totalMoney);
+
+  return totalMoney;
+}
+
+const chargeAndShowModal = (dataJson) =>{
+  showModal();
+  textPlate = document.querySelector('.text-plate-modal');
+  textMoney = document.querySelector('.text-money-modal');
+  
+  textPlate.innerHTML = dataJson.patente;
+  totalMoney= getTotalMoney(dataJson.timeIn,dataJson.timeOut,dataJson.type);
+  textMoney.innerHTML = "$ "+totalMoney;
+}
 
 const registerVehicle = (event) => {
   event.preventDefault();
 
-  let plateInput = document.getElementById("patente-reg");
-  let allVehicles = vehiclesArray.vehicles;
+  let allVehiclesJson = vehiclesArray.vehicles;
   let flag = false;
 
-  if  (allVehicles.length == 0){
-    alertMsj(plateInput.value);
+  if  (allVehiclesJson.length == 0){
+    alertMsj(plateInputReg.value,"no está registrado en el ingreso");
   }else{ 
-    for (let index = 0; index < allVehicles.length; index++) {
-      const vehicle = allVehicles[index];
-      if (vehicle.patente == plateInput.value){ 
+    for (let index = 0; index < allVehiclesJson.length; index++) {
+      const vehicle = allVehiclesJson[index];
+      if (vehicle.patente == plateInputReg.value){ 
         flag = true;
-        let tipo       = vehicle.type ;  // necesito el tipo para saber que tarifa cobrar
-        let timeOut    = moment().format("H:mm");
-        let dataFormat = {"patente" : plateInput.value,"type" : tipo, "timeOut" : timeOut};
+        let tipo = vehicle.type ;  
+        let timeIn = vehicle.timeIn ; 
+        let timeOut = moment().format("H:mm");
 
-        // aca va el modal, si el modal es confirmado , hace lo de abajo, de lo contrario NO
-        // el modal debe : 
-        //                Traer el precio segun el tiempo y el tipo de vehiclo
-
-
-        renderCount(dataFormat,-1);
-        makeItem(dataFormat,true); 
-        vehiclesArray.registerOut.push(dataFormat);
-        allVehicles.splice(index,1);
-    
-        let liToDelete = document.querySelector(`.${vehicle.patente}`);
-        liToDelete.parentElement.remove();
-        saveJson();
-        plateInput.value="";
-        plateInput.focus();
+        dataFormatOutJson ={"patente" : plateInputReg.value,
+                            "type" : tipo,
+                            "timeOut" : timeOut ,
+                            "timeIn":timeIn};
+        auxIndex= index;
+        
+        chargeAndShowModal(dataFormatOutJson);
+        // chargeModal = 
+        // aca estaba el  deleteAndRegisterItem();
         break;
       } 
     }
   }
-  
-  if (!flag){ alertMsj(plateInput.value); }
+  if (!flag){ alertMsj(plateInputReg.value,"no está registrado en el ingreso"); }
+}
 
+
+let confirmButtonModalRegister= () =>{
+  deleteAndRegisterItem(dataFormatOutJson);
+  let totalDOM = document.querySelector('.number-total');
+  console.log(totalDOM);
+  numParsed = parseInt(totalDOM.innerHTML)+totalMoney;
+  console.log(numParsed);
+  totalDOM.innerHTML = numParsed;
+  hideModal();
+}
+
+// guarde el cobro
+// guarde en el json el plate y el time out , el in y el type
+
+let deleteAndRegisterItem = () =>{
+  renderCount(dataFormatOutJson,-1);
+  makeItem(dataFormatOutJson,true); 
+  
+  let liToDelete = document.querySelector(`.${dataFormatOutJson.patente}`);
+  liToDelete.parentElement.remove();
+  
+  vehiclesArray.registerOut.push(dataFormatOutJson);
+  vehiclesArray.vehicles.splice(auxIndex,1);
+  saveJson();
+  
+  plateInputReg.value="";
+  plateInputReg.focus();
 }
 
 let initializeJsonData = () =>{
@@ -166,9 +258,7 @@ let initializeJsonData = () =>{
     renderCount(itemJson); // xjfw2
     makeItem(itemJson);
   });
-  vehiclesArray.registerOut.forEach(regJson =>{
-    makeItem(regJson,true);
-  });
+  vehiclesArray.registerOut.forEach(regJson =>{ makeItem(regJson,true); });
 }
 
 initializeJsonData();
